f565a6a35d7d8120eedde3bdb04bfaa59d7d0ba4
mjaylower <mlower@students.mchenry.edu>
2018-08-28 15:05:31 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// define variables
+        byte numByte = 77;
+        short numShort = -3420;
+        int numInt = 42;
+        long numLong = 450392;
+        float numFloat = 3.145f;
+        double numDouble = 53.235d;
+        boolean bool = true;
+        char wordChar = 'C';
+
+    // print lines
+        System.out.println("Byte: " + numByte);
+        System.out.println("Short: " + numShort);
+        System.out.println("Integer: " + numInt);
+        System.out.println("Long: " + numLong);
+        System.out.println("Float: " + numFloat);
+        System.out.println("Double: " + numDouble);
+        System.out.println("Boolean: " + bool);
+        System.out.println("Character: " + wordChar);
+    }
+}
